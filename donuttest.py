bfb0e387f0eee516fb42c8c2b39715933a1462c4
xknxknqq <1553730177@qq.com>
2018-05-06 16:21:36 +0800

    win workspace

---
+++ donuttest.py
@@ -0,0 +1,8 @@
+import binvox_rw
+
+with open('donut.binvox', 'rb') as f:
+    model = binvox_rw.read_as_3d_array(f)
+print(model.dims)
+print(model.scale)
+print(model.translate)
+print(model.data)
