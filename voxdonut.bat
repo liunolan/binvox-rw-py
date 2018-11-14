14300c4371931ac82bf30bf13204aa19646c2a84
Kainian Xie <1553730177@qq.com>
2018-11-14 20:04:44 +0800

    Update voxdonut.bat

--- voxdonut.bat
+++ voxdonut.bat
@@ -3,7 +3,7 @@ del donut.binvox
 
 rem use default resolution of 256 and downsample twice, so resolution is 64
 rem you will to add your own arguments here - see http://www.patrickmin.com/minecraft/
-binvox donut.obj -down -down -down
+binvox donut.stl -down -down -down
 
 rem now display it
 viewvox donut.binvox
